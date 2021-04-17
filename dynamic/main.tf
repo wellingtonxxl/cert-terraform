b63aa709f6a8dd471da20538a0276190dbb23b41
Wellington Rodrigues <wellington.acr@gmail.com>
2021-04-16 20:03:54 -0400

    Add dynamic blocks

---
+++ dynamic/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "ingress_rules" {
+  type = list(number)
+  default = [25, 80, 443, 8080, 8443]
+}
+
+variable "egress_rules" {
+  type = list(number)
+  default = [443, 8443]
+}
+
+resource "aws_instance" "myec2ws" {
+    ami = ""   
+    instance_type = "t2.micro"
+    security_groups = [ aws_security_group.web_traffic.name ]
+    tags = {
+        Name = "Web Server"
+    } 
+}
+
+resource "aws_security_group" "web_traffic" {
+    name = "Secure Server"
+    dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress_rules
+    content {
+        from_port = port.value
+        to_port = port.value
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  }
+
+    dynamic "egress" {
+    iterator = port
+    for_each = var.egress_rules
+    content {
+        from_port = port.value
+        to_port = port.value
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
