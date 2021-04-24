edc1560db98da5cd92c923dfa316a962b7e8e584
Wellington Rodrigues <wellington.acr@gmail.com>
2021-04-24 12:48:50 -0400

    Add Multiples Providers

--- providers/main.tf
+++ providers/main.tf
@@ -2,15 +2,16 @@ provider "aws" {
     region = "us-east-1"
 }
 
-provider "vault" {
-  
-}
-
-resource "vault_generic_secret" "mypassword" {
-  
+provider "aws" {
+    region = "us-east-2"
+    alias = "ohio"
 }
 
 resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_vpc" "ohiovpc" {
+    cidr_block = "10.0.0.0/16"
+    provider = aws.ohio
+}
